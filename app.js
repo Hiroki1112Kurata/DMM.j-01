970d14b3ed69ed41fd0565cb0e0d0c762236552f
Hiroki1112Kurata <sequoia1112@yahoo.co.jp>
2021-07-07 17:19:42 +0900

    [Start]j-01,02

---
+++ app.js
@@ -0,0 +1,71 @@
+// alert(4 + 3);
+
+// let str1 = 'Hello'
+// let str2 = 'World!!'
+// alert(str1 + str2)
+
+// if文ここから
+// let orenge = 100;
+// let apple = 100;
+
+// if (orenge < apple) {
+//   alert('みかんの値段がリンゴより安い');
+// } else if (orenge == apple) {
+//   alert('みかんとりんごが同じ値段');
+// } else {
+//   alert('みかんの値段がリンゴより高い');
+// }
+// if文ここまで
+
+
+// while文ここから
+// let max = 100;
+// let nom = 1;
+// let count = 0;
+// let mul = 5;
+
+// while(nom < max){
+//   nom = nom * mul;
+//   count = count + 1;
+// }
+
+// alert(mul + 'を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+// while文ここまで
+
+
+// for文ここから
+// let i;
+// let nom = 0;
+// let max = 15
+
+// for (i = 1; i < max + 1; i++) {
+//   nom = nom + i;
+// }
+
+// alert(
+//   '1から' + max + 'まで足した結果は' + nom + '\nです');
+
+// 解説: i++ == i = i + 1
+// for文ここまで
+
+
+// j - 2練習問題ここから
+// let nom1 = 2;
+// let nom2 = nom1 + 2;
+
+// let add = nom2 + nom1;
+// let sub = nom2 - nom1;
+// let mul = nom2 * nom1;
+// let div = nom2 / nom1;
+
+// alert(
+//   '四則演算' + '\n' +
+//   nom2 + '+' + nom1 + '=' + add + '\n' +
+//   nom2 + '-' + nom1 + '=' + sub + '\n' +
+//   nom2 + '×' + nom1 + '=' + mul + '\n' +
+//   nom2 + '÷' + nom1 + '=' + div
+//   );
+
+  // j - 2練習問題ここまで
+
+
